aeaef942a892330240e72bf26e4657f4748d0593
macall <macall@qq.com>
2016-01-15 19:23:51 +0800

    incharge

---
+++ sjmapi/lib/balance.action.php
@@ -0,0 +1,31 @@
+<?php
+
+class balance {
+
+    public function index() {
+        $root = array();
+
+        $user_data = $GLOBALS['user_info'];
+        $user_id = intval($user_data['id']);
+        if ($user_id == 0) {
+            $root['user_login_status'] = 0;
+            $root['info'] = "请先登陆";
+        } else {
+            $money = $GLOBALS['db']->getOne("select money from " . DB_PREFIX . "user where id = " . $user_id);
+            
+            $incharge_list = $GLOBALS['db']->getAll("SELECT pay_amount,create_time FROM " . DB_PREFIX . "deal_order WHERE TYPE=1 AND user_id = " . $user_id);
+            $root['money'] = $money;
+            foreach ($incharge_list as $key => $value) {
+                $value['create_time'] = date('Y-m-d H:i:s',$value['create_time']);
+                $incharge_list[$key] = $value;
+            }
+            $root['incharge_list'] = $incharge_list;
+        }
+
+        $root['page_title'] = '账户余额';
+        output($root);
+    }
+
+}
+
+?>
