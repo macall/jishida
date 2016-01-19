dd057cd02d97b55cf3c1337473d1af86061428e3
macall <macall@qq.com>
2016-01-19 15:23:16 +0800

    url update

--- public/runtime/data/sjmapi/c8/cc/~@7c020f42cfd0258dbe2120ad6dfbfb49.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000300a:3:{i:0;a:3:{s:2:"id";s:2:"21";s:4:"name";s:36:"桥亭活鱼小镇（万象城店）";s:7:"preview";s:74:"http://www.jsd.cm/public/attachment/201502/25/14/54ed67b2cd14b_194x118.jpg";}i:1;a:3:{s:2:"id";s:2:"23";s:4:"name";s:24:"肯德基（省府店）";s:7:"preview";s:74:"http://www.jsd.cm/public/attachment/201502/25/14/54ed6b92110bc_194x118.jpg";}i:2;a:3:{s:2:"id";s:2:"25";s:4:"name";s:33:"盛世经典牛排（宝龙店）";s:7:"preview";s:74:"http://www.jsd.cm/public/attachment/201502/25/14/54ed6f616ffc5_194x118.jpg";}}
-?>
